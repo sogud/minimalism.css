2ef2eee100db5afc7392243fbf194edcd2b62103
shunshun <lwwtotod@gmail.com>
2019-12-31 09:41:02 +0800

    🗯chore(Update commit style):

--- .cz-config.js
+++ .cz-config.js
@@ -4,39 +4,39 @@ module.exports = {
 
   types: [
     {
-      value: 'WIP',
+      value: '💪WIP',
       name : '💪  WIP:      工作正在进行中 Work in progress'
     },
     {
-      value: 'feat',
+      value: '✨feat',
       name : '✨  feat:     新功能 A new feature'
     },
     {
-      value: 'fix',
+      value: '🐞fix',
       name : '🐞  fix:      修复BUG A bug fix'
     },
     {
-      value: 'refactor',
+      value: '🛠refactor',
       name : '🛠  refactor: 没有修复错误也未添加功能的代码 A code change that neither fixes a bug nor adds a feature'
     },
     {
-      value: 'docs',
+      value: '📚docs',
       name : '📚  docs:     仅文档更改 Documentation only changes'
     },
     {
-      value: 'test',
+      value: '🏁test',
       name : '🏁  test:     添加缺失的测试或更正现有的测试 Add missing tests or correcting existing tests'
     },
     {
-      value: 'chore',
+      value: '🗯chore',
       name : '🗯  chore:    不会修改src或测试文件的更改。例如更新构建任务，程序包管理器 Changes that don\'t modify src or test files. Such as updating build tasks, package manager'
     },
     {
-      value: 'style',
+      value: '💅style',
       name : '💅  style:    代码样式，不影响代码含义的更改（空格，格式，缺少分号等） Code Style, Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)'
     },
     {
-      value: 'revert',
+      value: '⏪revert',
       name : '⏪  revert:   提交回滚 Revert to a commit'
     }
   ],
